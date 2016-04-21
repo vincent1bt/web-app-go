3f1f84008dc3f118fc35c1ae4d9200be531f796b
Vicente <vincent_97@live.com.mx>
2016-04-21 15:55:05 -0500

    Errores en nombres arreglada, descripcion actualizada

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #Código de la app web hecha en go#
 
-[Link](http://vincentblog.me) al tutorial
+[Link](http://vincentblog.me/posts/como-crear-una-web-app-sencilla-en-go) al tutorial
--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -9,12 +9,12 @@ import (
 
 //creamos las funciones que corresponden a las rutas
 func Index(w http.ResponseWriter, r *http.Request) {
-  t, _ := template.ParseFiles("views/movies/index.html") //indicamos la ruta del template/html
+  t, _ := template.ParseFiles("views/users/index.html") //indicamos la ruta del template/html
   t.Execute(w, nil) //se hace render del template
 }
 
 func Show(w http.ResponseWriter, r *http.Request) {
   user := &userModel.User{ Name: "Alberto", Email: "albert@email.com" } //creamos un nuevo usuario
-  t, _ := template.ParseFiles("views/movies/show.html") //indicamos a ruta del template
+  t, _ := template.ParseFiles("views/users/show.html") //indicamos a ruta del template
   t.Execute(w, user) //le pasamos al usuario al template para que este disponible
 }
