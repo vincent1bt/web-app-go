05f82f040ab746652bf16f2a33a89c1a442ce31f
Vicente <vincent_97@live.com.mx>
2016-04-21 15:46:35 -0500

    App finalizada, con templates en html

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,14 +3,18 @@ package usersController
 
 import (
   "net/http"
-  "fmt"
+  "html/template" //agregamos la libreria de templates
+  "projects/webApp/models/user"
 )
 
 //creamos las funciones que corresponden a las rutas
 func Index(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Index users")
+  t, _ := template.ParseFiles("views/movies/index.html") //indicamos la ruta del template/html
+  t.Execute(w, nil) //se hace render del template
 }
 
 func Show(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Show users")
+  user := &userModel.User{ Name: "Alberto", Email: "albert@email.com" } //creamos un nuevo usuario
+  t, _ := template.ParseFiles("views/movies/show.html") //indicamos a ruta del template
+  t.Execute(w, user) //le pasamos al usuario al template para que este disponible
 }
---
+++ models/user/user.go
@@ -0,0 +1,6 @@
+package userModel
+
+type User struct {
+  Name string
+  Email string
+}
